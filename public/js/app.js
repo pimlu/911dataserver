d162edf1d99035500020eb0126e9015dc364ed28
pimlu <pimlu@users.noreply.github.com>
2016-02-27 22:52:05 -0500

    create basic API/user interface
    tested my API with cURL.  a lot of work left to do on the user
    interface.

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,9 @@
+var UPDATE_RATE = 2000;
+
+function getToken() {
+  return localStorage.token;
+}
+
 angular.module('app', ['ngRoute', 'components'])
 .config(function($routeProvider) {
   //create our 'pages' and associate controllers with them
@@ -14,11 +20,45 @@ angular.module('app', ['ngRoute', 'components'])
 //controllers for each main section of our app
 .controller('AppCtrl', ['$scope', function($scope) {
 }])
-.controller('DashCtrl', ['$scope', function($scope) {
-  $scope.calls = [123,456].map(function(num) { return {number:num}; });
-}])
-.controller('CallCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
-  $scope.call = {
-    number: $routeParams.number
+.controller('DashCtrl', ['$scope', '$http', '$timeout', '$q',
+  function($scope, $http, $timeout, $q) {
+  var cancel, callt;
+  //once per UPDATE_RATE, make an http request to monitor the call list
+  var callfn = function() {
+    cancel = $q.defer();
+    callt = $timeout(UPDATE_RATE);
+    var req = $http.post('/api/auth/calls', {
+      token: getToken()
+    }, { timeout: cancel }).then(function(res) {
+      $scope.calls = res.data.calls;
+    });
+    $q.all([callt, req]).then(callfn);
   };
+  callfn();
+  $scope.calls = null;
+  //cancel requests/timers
+  $scope.$on('destroy', function(evt) {
+    $timeout.cancel(callt);
+    cancel && cancel.resolve();
+  });
+}])
+.controller('CallCtrl', ['$scope', '$routeParams', '$http', '$q',
+  function($scope, $routeParams, $http, $q) {
+  var number = $routeParams.number;
+  $scope.call = { number: number };
+  var cancel = $q.defer();
+  //TODO updates?
+  var req = $http.post('/api/auth/call', {
+    token: getToken(),
+    number: number
+  }, { timeout: cancel }).then(function(res) {
+    $scope.call.data = res.data.data;
+  }, function(e) {
+    $scope.call.fail = e.status <= 0 ? 'The requested call couldn\'t be loaded.' :
+      `The number ${number} doesn't have an active session with the server.`;
+  });
+  
+  $scope.$on('destroy', function(evt) {
+    cancel && cancel.resolve();
+  });
 }]);
--- src/api.js
+++ src/api.js
@@ -1,22 +1,88 @@
-var express = require('express');
-var router = express.Router();
+const config = require('../config.js');
 
-var calls = {};
+const _ = require('lodash');
+const express = require('express');
+const router = express.Router();
 
+const calls = {};
 //API endpoints for responders and callers
 
 router.get('/test', function(req, res) {
   res.end('test');
 });
 
+/*setInterval(function() {
+  console.log(calls);
+}, 5000);*/
+
+//What a Terrible Failure
+function wtf(res, status) {
+  res.status(status || 400).json({
+    success: false,
+    error: 'wot r u doin'
+  });
+}
+//registers a new call with the system
+function beginCall(body) {
+  const num = body.number;
+  const call = calls[num] = {
+    number: num,
+    data: body.data || {},
+    pump: _.debounce(function() {
+      //if our call is gone, quit
+      if(calls[num] !== call) return;
+      endCall(num, 'Connection timed out.');
+    }, config.TIMEOUT)
+  };
+  call.pump();
+  return { success: true };
+}
+//removes a call from the system
+function endCall(num, reason) {
+  if(!calls[num]) return;
+  calls[num].pump.cancel();
+  delete calls[num];
+  return { success: true };
+}
+//initiate a call
 router.post('/911', function(req, res) {
   var body = req.body;
   var num = body.number;
-  if(calls[num]) {
-    clearTimeout(calls[num].tid);
-    delete calls[num];
-  }
-  
+  if(!num || calls[num]) return wtf(res);
+  res.json(beginCall(body));
+});
+//keep a call alive
+router.post('/heartbeat', function(req, res) {
+  var num = req.body.number;
+  if(!num || !calls[num]) return wtf(res);
+  //if there's a reason, they're ending rather than continuing
+  if(req.body.reason) return res.json(endCall(req.body.reason));
+  calls[num].pump();
+  res.json({success: true});
+});
+//require auth token for this area
+router.use('/auth/*', function(req, res, next) {
+  if(req.body.token !== config.API_TOKEN) return wtf(res, 401);
+  next();
+});
+//list of active calls
+router.post('/auth/calls', function(req, res) {
+  res.json({
+    success: true,
+    calls: Object.keys(calls).map(function(number) {
+      return { number: number };
+    })
+  });
+});
+//specific data on a single call
+router.post('/auth/call', function(req, res) {
+  const call = calls[req.body.number];
+  if(!call) return wtf(res);
+  res.json({
+    success: true,
+    number: call.number,
+    data: call.data
+  });
 });
 
 module.exports = router;
